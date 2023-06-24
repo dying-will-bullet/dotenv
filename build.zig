77637b37a4957fba0a3e02bc23edb954a50a6a22
Hanaasagi <ambiguous404@gmail.com>
2023-06-24 10:44:37 +0900

    chore: remove unused code

--- build.zig
+++ build.zig
@@ -41,37 +41,6 @@ pub fn build(b: *std.Build) void {
 
     const run_main_tests = b.addRunArtifact(main_tests);
 
-    // const parser_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/parser.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // parser_tests.linkSystemLibrary("c");
-
-    // const run_parser_tests = b.addRunArtifact(parser_tests);
-
-    // const utils_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/utils.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // utils_tests.linkSystemLibrary("c");
-    // const run_utils_tests = b.addRunArtifact(utils_tests);
-
-    // const loader_tests = b.addTest(.{
-    //     .root_source_file = .{ .path = "src/loader.zig" },
-    //     .target = target,
-    //     .optimize = optimize,
-    // });
-    // loader_tests.linkSystemLibrary("c");
-    // const run_loader_tests = b.addRunArtifact(loader_tests);
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build test`
-    // This will evaluate the `test` step rather than the default, which is "install".
     const test_step = b.step("test", "Run library tests");
     test_step.dependOn(&run_main_tests.step);
-    // test_step.dependOn(&run_parser_tests.step);
-    // test_step.dependOn(&run_utils_tests.step);
-    // test_step.dependOn(&run_loader_tests.step);
 }
