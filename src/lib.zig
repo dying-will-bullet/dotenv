3a432b0b697182c897ac6327690c6ea4190b5349
Hanaasagi <ambiguous404@gmail.com>
2023-06-24 19:23:06 +0900

    chore: rename some variables

--- src/lib.zig
+++ src/lib.zig
@@ -38,36 +38,36 @@ pub fn loadFrom(allocator: std.mem.Allocator, path: []const u8, options: Options
 }
 
 test "test load real file" {
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR1") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR2") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR3") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR4") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR5") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR6") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR7") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR8") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR9") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR10") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_VAR11") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_MULTILINE1") == null);
-    try testing.expect(std.os.getenv("CODEGEN_TEST_MULTILINE2") == null);
+    try testing.expect(std.os.getenv("VAR1") == null);
+    try testing.expect(std.os.getenv("VAR2") == null);
+    try testing.expect(std.os.getenv("VAR3") == null);
+    try testing.expect(std.os.getenv("VAR4") == null);
+    try testing.expect(std.os.getenv("VAR5") == null);
+    try testing.expect(std.os.getenv("VAR6") == null);
+    try testing.expect(std.os.getenv("VAR7") == null);
+    try testing.expect(std.os.getenv("VAR8") == null);
+    try testing.expect(std.os.getenv("VAR9") == null);
+    try testing.expect(std.os.getenv("VAR10") == null);
+    try testing.expect(std.os.getenv("VAR11") == null);
+    try testing.expect(std.os.getenv("MULTILINE1") == null);
+    try testing.expect(std.os.getenv("MULTILINE2") == null);
 
     try loadFrom(testing.allocator, "./testdata/.env", .{});
 
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR1").?, "hello!");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR2").?, "'quotes within quotes'");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR3").?, "double quoted with # hash in value");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR4").?, "single quoted with # hash in value");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR5").?, "not_quoted_with_#_hash_in_value");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR6").?, "not_quoted_with_comment_beheind");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR7").?, "not quoted with escaped space");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR8").?, "double quoted with comment beheind");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR9").?, "Variable starts with a whitespace");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR10").?, "Value starts with a whitespace after =");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_VAR11").?, "Variable ends with a whitespace before =");
-    try testing.expectEqualStrings(std.os.getenv("CODEGEN_TEST_MULTILINE1").?, "First Line\nSecond Line");
+    try testing.expectEqualStrings(std.os.getenv("VAR1").?, "hello!");
+    try testing.expectEqualStrings(std.os.getenv("VAR2").?, "'quotes within quotes'");
+    try testing.expectEqualStrings(std.os.getenv("VAR3").?, "double quoted with # hash in value");
+    try testing.expectEqualStrings(std.os.getenv("VAR4").?, "single quoted with # hash in value");
+    try testing.expectEqualStrings(std.os.getenv("VAR5").?, "not_quoted_with_#_hash_in_value");
+    try testing.expectEqualStrings(std.os.getenv("VAR6").?, "not_quoted_with_comment_beheind");
+    try testing.expectEqualStrings(std.os.getenv("VAR7").?, "not quoted with escaped space");
+    try testing.expectEqualStrings(std.os.getenv("VAR8").?, "double quoted with comment beheind");
+    try testing.expectEqualStrings(std.os.getenv("VAR9").?, "Variable starts with a whitespace");
+    try testing.expectEqualStrings(std.os.getenv("VAR10").?, "Value starts with a whitespace after =");
+    try testing.expectEqualStrings(std.os.getenv("VAR11").?, "Variable ends with a whitespace before =");
+    try testing.expectEqualStrings(std.os.getenv("MULTILINE1").?, "First Line\nSecond Line");
     try testing.expectEqualStrings(
-        std.os.getenv("CODEGEN_TEST_MULTILINE2").?,
+        std.os.getenv("MULTILINE2").?,
         "# First Line Comment\nSecond Line\n#Third Line Comment\nFourth Line\n",
     );
 }
