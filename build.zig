b7c0481bac6b897c38a8b051cbe3579584da28c7
Tristan Murphy <72839119+HyperCodec@users.noreply.github.com>
2024-10-27 07:04:36 -0400

    fix some deprecation changes in new zig versions (#4)
    * replace deprecated union field in build.zig
    
    * change os.getenv to posix.getenv

--- build.zig
+++ build.zig
@@ -16,14 +16,14 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("dotenv", .{
-        .root_source_file = .{ .path = "src/lib.zig" },
+        .root_source_file = b.path("src/lib.zig"),
     });
 
     const lib = b.addStaticLibrary(.{
         .name = "dotenv",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "src/lib.zig" },
+        .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -37,7 +37,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const main_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/lib.zig" },
+        .root_source_file = b.path("src/lib.zig"),
         .target = target,
         .optimize = optimize,
     });
